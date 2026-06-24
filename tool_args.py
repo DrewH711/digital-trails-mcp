b3d35f286839bea1c17dde48db97faac62c877c1
Drew Holland <drexh47127@gmail.com>
2026-06-24 14:32:01 -0400

    Add get_protocol_file_list and get_file_contents tools

--- tool_args.py
+++ tool_args.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field, field_validator
 from typing import Literal, Optional
+import os
 
 available_protocols = Literal["protocol-uma","protocol-leia","mindtrails_movement","mindtrails_spanish","github-mcp-test"]
 
@@ -11,11 +12,20 @@ class protocolArgs(BaseModel):
         description=f"The protocol to clone. Options are: {literal_to_str(available_protocols)}"
         )
     
-class releaseProtocolArgs(BaseModel):
+class readProtocolArgs(BaseModel):
     protocol_name: available_protocols = Field(
-        description=f"The protocol to clone. Options are: {literal_to_str(available_protocols)}"
+        description=f"The protocol to read files from. Options are: {literal_to_str(available_protocols)}"
         )
     
+    file_paths: list[str] = Field(
+        description = "List of file paths to read"
+    )
+
+    @field_validator('file_paths', mode='before')
+    @classmethod
+    def validate_file_paths(cls, path_list: list[str]):
+        return [path.replace("\\", "/") for path in path_list if os.access(path, mode=0)]
+        
 class latestOrPrerelease(BaseModel):
     latest_or_prerelease: Literal['Latest','Prerelease'] = Field(
         title="Should this release be marked as latest or as a prerelease?"
